119f3a305e5a52167522e04be88c9e427a055568
unknown <kellskelechi@gmail.com>
2022-03-14 23:46:46 +0100

    alan testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import alanBtn from "@alan-ai/alan-sdk-web"
 
+
+
+const alanKey='0bfa95d4d552d4459b78fc0ee3cfc7342e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+
+    useEffect(() => {
+        alanBtn ({
+            key:alanKey,
+            onCommand: (command )=>{
+                if (command === 'testCommand') {
+                    alert ('This cod works');
+                }
+            }
+
+        })
+    }, [])
   return (
-    <div>App</div>
+    <div>
+        <h1>
+            happy
+        </h1>
+    </div>
   )
 }
 
